91e1ac99d0cf9a0e9fb582fc078033027f0a3727
Feliphe Xavier <feliphexavier2604@gmail.com>
2024-02-01 14:52:59 -0300

    implementacao do responseEntity

--- src/main/java/com/restaurante/cardapio/controllers/FoodController.java
+++ src/main/java/com/restaurante/cardapio/controllers/FoodController.java
@@ -3,8 +3,9 @@ package com.restaurante.cardapio.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,40 +28,58 @@ public class FoodController {
     @Autowired
     private FoodRepository repository;
 
+    @CrossOrigin(origins = "*",allowedHeaders = "*")
     @GetMapping
-    public List<FoodDTO> getAll(){
-        List<FoodDTO> foodList = repository.findAll().stream().map(FoodDTO::new).toList();
-        return foodList;
-    }
-    @ExceptionHandler
-    @GetMapping("{id}")
-    public Food getById(@PathVariable Long id){
-        return repository.findById(id).orElseThrow(()->new NotFound("id is invalid" + id));
-    } 
-    
-    @PostMapping
-    public void createFood(@RequestBody FoodDTO data){
-        Food food = new Food(data);
-        repository.save(food);
-        return;
+    public ResponseEntity<List<FoodDTO>> getAll(){
+        var foodList = repository.findAll().stream().map(FoodDTO::new).toList();
+        return ResponseEntity.ok(foodList);
     }
 
+
+    @CrossOrigin(origins = "*",allowedHeaders = "*")
+    @GetMapping("{id}")
+    public ResponseEntity<FoodDTO> getById(@PathVariable Long id){
+        var food =  repository.findById(id).orElseThrow(()->new NotFound("id is invalid"));
+        return ResponseEntity.ok(new FoodDTO(food));
+
+    }
+    
+    
+    @CrossOrigin(origins = "*",allowedHeaders = "*")
+    @PostMapping
+    public ResponseEntity<Void> createFood(@RequestBody FoodDTO data){
+        Food food = new Food(data);
+        repository.save(food);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @CrossOrigin(origins = "*",allowedHeaders = "*")
     @PutMapping("{id}")
-    public FoodDTO updateFood(@PathVariable Long id,@RequestBody @Valid FoodDTO data){
+    public ResponseEntity<FoodDTO> updateFood(@PathVariable Long id,@RequestBody @Valid FoodDTO data){
         Food food = repository.findById(id).orElseThrow(()->new NotFound("id is invalid"));
         food.setName(data.name());
         food.setDescription(data.description());
         food.setPhoto(data.photo());
         food.setPrice(data.price());
         Food uptadedNewFood = repository.save(food);
-        return new FoodDTO(uptadedNewFood); 
+        return ResponseEntity.ok(new FoodDTO(uptadedNewFood)); 
     }
+
+
+
+    @CrossOrigin(origins = "*",allowedHeaders = "*")
     @DeleteMapping("{id}")
-    public void deleteFood(@PathVariable Long id){
+    public ResponseEntity<Void> deleteFood(@PathVariable Long id){
         repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
+
+
+    @CrossOrigin(origins = "*",allowedHeaders = "*")
     @DeleteMapping("/deleteAll")
-    public void deleteAllFoods(){
+    public ResponseEntity<Void> deleteAllFoods(){
         repository.deleteAll();
+        return ResponseEntity.noContent().build();
     }
 }
